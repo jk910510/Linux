3b42ab05554d429d6b53bfae4eb7326d69f83fde
jk9 <jk910510@163.com>
2019-12-31 14:50:36 +0800

    1_chrdevbase

---
+++ linux/imx6ull_dever/1_chrdevbase/.chrdevbase.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jk/linux/imx6ull_device/1_chrdevbase/chrdevbase.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/jk/linux/imx6ull_device/1_chrdevbase/chrdevbase.ko /home/jk/linux/imx6ull_device/1_chrdevbase/chrdevbase.o /home/jk/linux/imx6ull_device/1_chrdevbase/chrdevbase.mod.o
---
+++ linux/imx6ull_dever/1_chrdevbase/.tmp_versions/chrdevbase.mod
@@ -0,0 +1,2 @@
+/home/jk/linux/imx6ull_device/1_chrdevbase/chrdevbase.ko
+/home/jk/linux/imx6ull_device/1_chrdevbase/chrdevbase.o
